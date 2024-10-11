7f9a0440c0c1aef7a1501c6924774049d86c0842
GloRosado07/wdd230 <address>
2024-10-10 18:11:20 -0600

    error week3

--- server.js
+++ server.js
@@ -21,6 +21,11 @@ app.set("view engine", "ejs")
 app.use(expressLayouts)
 app.set("layout", "./layouts/layout") // not at views root
 
+/* ***********************
+ * Routes
+ *************************/
+app.use(static)
+
 //Index Route
 app.get("/", baseController.buildHome)
 
@@ -28,10 +33,10 @@ app.get("/", baseController.buildHome)
 app.use("/inv", inventoryRoute)
 
 
-/* ***********************
- * Routes
- *************************/
-app.use(static)
+// File Not Found Route - must be last route in list
+app.use(async (req, res, next) => {
+  next({status: 404, message: 'Sorry, we appear to have lost that page.'})
+})
 
 /* ***********************
  * Local Server Information
@@ -39,6 +44,21 @@ app.use(static)
  *************************/
 const port = process.env.PORT
 const host = process.env.HOST
+
+
+/* ***********************
+* Express Error Handler
+* Place after all other middleware
+*************************/
+app.use(async (err, req, res, next) => {
+  let nav = await utilities.getNav()
+  console.error(`Error at: "${req.originalUrl}": ${err.message}`)
+  res.render("errors/error", {
+    title: err.status || 'Server Error',
+    message: err.message,
+    nav
+  })
+})
 
 /* ***********************
  * Log statement to confirm server operation
