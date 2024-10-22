66089636fcff53a425f354fcede788889d025c51
GloRosado07/wdd230 <address>
2024-10-21 22:10:53 -0600

    updates

--- controllers/invController.js
+++ controllers/invController.js
@@ -162,4 +162,27 @@ invCont.addVehicle = async function (req, res, next) {
   }
 }
 
+/* ***************************
+ *  Bad function
+ * ************************** */
+invCont.badFunction = async function (req, res, next) {
+  res.render("./inventory/inexistent", {
+    title:inexistent
+  })
+}
+
+
+/* ***************************
+ *  Return Inventory by Classification As JSON
+ * ************************** */
+invCont.getInventoryJSON = async (req, res, next) => {
+  const classification_id = parseInt(req.params.classification_id)
+  const invData = await invModel.getInventoryByClassificationId(classification_id)
+  if (invData[0].inv_id) {
+    return res.json(invData)
+  } else {
+    next(new Error("No data returned"))
+  }
+}
+
   module.exports = invCont
